4246b6e8ab9895c7d92e7c77ce0bb4176648a6cb
Marius Bughiu <marius.bughiu@gmail.com>
2023-10-30 22:33:07 +0200

    Add UnsafeAccessor example

---
+++ unsafe-accessor/UnsafeAccessor/Foo.cs
@@ -0,0 +1,19 @@
+﻿namespace UnsafeAccessor;
+
+class Foo
+{
+    private Foo() { }
+    private Foo(string value)
+    {
+        InstanceProperty = value;
+    }
+
+    private string InstanceProperty { get; set; } = "instance-property";
+    private static string StaticProperty { get; set; } = "static-property";
+
+    private int instanceField = 1;
+    private static int staticField = 2;
+
+    private string InstanceMethod(int value) => $"instance-method:{value}";
+    private static string StaticMethod(int value) => $"static-method:{value}";
+}
---
+++ unsafe-accessor/UnsafeAccessor/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.CompilerServices;
+using UnsafeAccessor;
+
+// Creating object instances using private constructors
+
+[UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+extern static Foo CallPrivateConstructor();
+
+[UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+extern static Foo CallPrivateConstructorWithParameters(string value);
+
+var instance1 = CallPrivateConstructor();
+var instance2 = CallPrivateConstructorWithParameters("bar");
+
+
+
+// Invoke private instance methods
+
+[UnsafeAccessor(UnsafeAccessorKind.Method, Name = "InstanceMethod")]
+extern static string InstanceMethod(Foo @this, int value);
+
+Console.WriteLine(InstanceMethod(instance1, 42)); // "instance-method:42"
+
+
+
+// Get / Set private instance properties
+
+[UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_InstanceProperty")]
+extern static string InstanceGetter(Foo @this);
+
+[UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_InstanceProperty")]
+extern static void InstanceSetter(Foo @this, string value);
+
+Console.WriteLine(InstanceGetter(instance1)); // "instance-property"
+InstanceSetter(instance1, "bar");
+Console.WriteLine(InstanceGetter(instance1)); // "bar"
+
+
+
+// Static methods and properties
+
+[UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "StaticMethod")]
+extern static string StaticMethod(Foo @this, int value);
+
+[UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "get_StaticProperty")]
+extern static string StaticGetter(Foo @this);
+
+[UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "set_StaticProperty")]
+extern static void StaticSetter(Foo @this, string value);
+
+
+
+// Fields
+
+[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "instanceField")]
+extern static ref int InstanceField(Foo @this);
+
+[UnsafeAccessor(UnsafeAccessorKind.StaticField, Name = "staticField")]
+extern static ref int StaticField(Foo @this);
+
+// Read the field value
+var x = InstanceField(instance1);
+var y = StaticField(instance1);
+
+// Update the field value
+InstanceField(instance1) = 3;
+StaticField(instance1) = 4;
