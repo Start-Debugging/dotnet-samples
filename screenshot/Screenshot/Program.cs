24d4273803c67824b2885b6f18cb8d535ec75657
Marius Bughiu <marius.bughiu@gmail.com>
2023-11-01 22:05:39 +0200

    Grab screenshot from dotnet core console app

---
+++ screenshot/Screenshot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+var screen = SystemInformation.VirtualScreen;
+
+int x = screen.Left;
+int y = screen.Top;
+int w = screen.Width;
+int h = screen.Height;
+
+var image = new Bitmap(w, h);
+using var graphics = Graphics.FromImage(image);
+graphics.CopyFromScreen(x, y, 0, 0, new Size(w, h));
+
+string picturesDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+string fileName = $"Screenshot {DateTime.Now:yyyy-MM-dd HHmmss}.png";
+string filePath = Path.Combine(picturesDirectory, fileName);
+
+image.Save(filePath, ImageFormat.Png);
+
+Console.WriteLine("Screenshot saved to: " + filePath);
