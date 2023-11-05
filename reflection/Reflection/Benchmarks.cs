5e6087a34b94a7d0979aa0354b2753b0795b4ded
Marius Bughiu <marius.bughiu@gmail.com>
2023-11-05 11:37:41 +0200

    Add GetGenericTypeDefinition benchmark

---
+++ reflection/Reflection/Benchmarks.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace UnsafeAccessor.Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net70)]
+[SimpleJob(RuntimeMoniker.Net80)]
+public class Benchmarks
+{
+    private readonly Type _type = typeof(List<int>);
+
+    [Benchmark]
+    public Type GetGenericTypeDefinition() => _type.GetGenericTypeDefinition();
+}
---
+++ reflection/Reflection/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using UnsafeAccessor.Benchmarks;
+
+namespace Reflection;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmarks>();
+
+        Console.ReadKey();
+    }
+}
